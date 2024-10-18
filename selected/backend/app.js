e110645b5b83295aec400bee8848b9ac3bb1b726
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-10-18 09:42:21 -0500

    Added certificate and code for DO SQL Connection.  Code is commented out.

--- selected/backend/app.js
+++ selected/backend/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const db = require('./db');
-const emailService = require('./email-service');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const db = require("./db");
+const emailService = require("./email-service");
 
 // Secret key for JWT signing (in production, store this securely)
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -12,200 +12,219 @@ const port = 5001;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    res.json({ message: 'Ready to accept requests!' });
+app.get("/", async (req, res) => {
+  // await db
+  // .connect()
+  // .then(async () => {
+  //   // query the databse for project records
+  //   polls = await db.getAllProjects();
+  //   polls.forEach((item) => {
+  //   // do stuff to data
+  //   });
+  res.json({ message: "Ready to accept requests!" });
 });
 
 // User login route (POST request to authenticate)
-app.post('/login', async (req, res) => {
-    const { email } = req.body;
+app.post("/login", async (req, res) => {
+  const { email } = req.body;
 
-    try {
-        const validUser = await db.findUserByEmail(email);
+  try {
+    const validUser = await db.findUserByEmail(email);
 
-        if (!validUser) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        // Generate One-Time-Passcode with 8 digits
-        const passcode = Math.floor(10000000 + Math.random() * 90000000);
-
-        // Save it to the database
-        await db.savePasscode(validUser.UserId, passcode);
-
-        // Send email with One-Time-Passcode
-        await emailService.sendEmail(email, passcode);
-
-        // Send response that passcode is sent to email
-        res.json({ message: 'Passcode sent to email' });
-    } catch (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
+    if (!validUser) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    // Generate One-Time-Passcode with 8 digits
+    const passcode = Math.floor(10000000 + Math.random() * 90000000);
+
+    // Save it to the database
+    await db.savePasscode(validUser.UserId, passcode);
+
+    // Send email with One-Time-Passcode
+    await emailService.sendEmail(email, passcode);
+
+    // Send response that passcode is sent to email
+    res.json({ message: "Passcode sent to email" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // User registration route (POST request to register)
-app.post('/register', async (req, res) => {
-    const user = req.body;
+app.post("/register", async (req, res) => {
+  const user = req.body;
 
-    try {
-        const existingUser = await db.findUserByEmail(user.emailId);
+  try {
+    const existingUser = await db.findUserByEmail(user.emailId);
 
-        if (existingUser) {
-            return res.status(409).json({ message: 'User already exists' });
-        }
-
-        // Save user to database
-        await db.saveUser(user);
-
-        res.json({ message: 'User registered' });
-    } catch (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
     }
+
+    // Save user to database
+    await db.saveUser(user);
+
+    res.json({ message: "User registered" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-app.post('/validate_otp', async (req, res) => {
-    const { email, passcode } = req.body;
+app.post("/validate_otp", async (req, res) => {
+  const { email, passcode } = req.body;
 
-    try {
-        const validUser = await db.findUserByEmail(email);
+  try {
+    const validUser = await db.findUserByEmail(email);
 
-        const isValid = await db.isPasscodeValid(validUser.UserId, passcode);
+    const isValid = await db.isPasscodeValid(validUser.UserId, passcode);
 
-        if (!isValid) {
-            return res.status(401).json({ message: 'Invalid OTP' });
-        }
-
-        // If OTP is valid, generate access token and send it in the response
-        const token = jwt.sign({ userId: validUser.UserId, emailId: validUser.EmailID }, JWT_SECRET, {
-            expiresIn: '1h' // Token expires in 1 hour
-        });
-
-        res.json({ token });
-    } catch (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
+    if (!isValid) {
+      return res.status(401).json({ message: "Invalid OTP" });
     }
+
+    // If OTP is valid, generate access token and send it in the response
+    const token = jwt.sign(
+      { userId: validUser.UserId, emailId: validUser.EmailID },
+      JWT_SECRET,
+      {
+        expiresIn: "1h", // Token expires in 1 hour
+      }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Extend the token validity
-app.post('/refresh_token', authenticateToken, (req, res) => {
-    const token = jwt.sign({ id: req.user.id, email: req.user.email }, JWT_SECRET, {
-        expiresIn: '1h' // Token expires in 1 hour
-    });
+app.post("/refresh_token", authenticateToken, (req, res) => {
+  const token = jwt.sign(
+    { id: req.user.id, email: req.user.email },
+    JWT_SECRET,
+    {
+      expiresIn: "1h", // Token expires in 1 hour
+    }
+  );
 
-    res.json({ token });
+  res.json({ token });
 });
 
 // Get all Active Polls
-app.get('/api/poll', authenticateToken, async (req, res) => {
-    const email = req.user.emailId;
+app.get("/api/poll", authenticateToken, async (req, res) => {
+  const email = req.user.emailId;
 
-    try {
-        const validUser = await db.findUserByEmail(email);
+  try {
+    const validUser = await db.findUserByEmail(email);
 
-        if (!validUser) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-
-        const polls = await db.getActivePolls();
-
-        res.json(polls);
-    } catch (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
+    if (!validUser) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    const polls = await db.getActivePolls();
+
+    res.json(polls);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Create a new Poll
-app.post('/api/poll', authenticateToken, async (req, res) => {
-    const newPoll = req.body;
+app.post("/api/poll", authenticateToken, async (req, res) => {
+  const newPoll = req.body;
 
-    // Validate the input
-    if (!newPoll.QuestionText || newPoll.QuestionText.length <= 10) {
-        return res.status(400).json({ message: 'Invalid Poll Question' });
+  // Validate the input
+  if (!newPoll.QuestionText || newPoll.QuestionText.length <= 10) {
+    return res.status(400).json({ message: "Invalid Poll Question" });
+  }
+
+  if (!newPoll.Options || newPoll.Options.length < 2) {
+    return res.status(400).json({ message: "At least 2 options are required" });
+  }
+
+  for (const option of newPoll.Options) {
+    if (!option || option.length == 0) {
+      return res.status(400).json({ message: "Invalid Option Text" });
     }
+  }
 
-    if (!newPoll.Options || newPoll.Options.length < 2) {
-        return res.status(400).json({ message: 'At least 2 options are required' });
-    }
+  if (!newPoll.ExpirationTime || newPoll.ExpirationTime <= new Date()) {
+    return res.status(400).json({ message: "Invalid Poll Expiration Date" });
+  }
 
-    for (const option of newPoll.Options) {
-        if (!option || option.length == 0) {
-            return res.status(400).json({ message: 'Invalid Option Text' });
-        }
-    }
+  try {
+    // Save to database
+    await db.savePoll(newPoll, req.user.userId);
 
-    if (!newPoll.ExpirationTime || newPoll.ExpirationTime <= new Date()) {
-        return res.status(400).json({ message: 'Invalid Poll Expiration Date' });
-    }
-
-    try {
-        // Save to database
-        await db.savePoll(newPoll, req.user.userId);
-
-        // Return the new Poll
-        res.status(201).json(newPoll);
-    } catch (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    // Return the new Poll
+    res.status(201).json(newPoll);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-app.put('/api/poll', authenticateToken, async (req, res) => {
-    const existingPoll = req.body;
+app.put("/api/poll", authenticateToken, async (req, res) => {
+  const existingPoll = req.body;
 
-    // Validate the input
-    if (!existingPoll.QuestionText || existingPoll.Options.length <= 10) {
-        return res.status(400).json({ message: 'Invalid Poll Question' });
+  // Validate the input
+  if (!existingPoll.QuestionText || existingPoll.Options.length <= 10) {
+    return res.status(400).json({ message: "Invalid Poll Question" });
+  }
+
+  if (existingPoll.Options.length < 2) {
+    return res.status(400).json({ message: "At least 2 options are required" });
+  }
+
+  for (const option of existingPoll.Options) {
+    if (!option.OptionText || option.OptionText.length == 0) {
+      return res.status(400).json({ message: "Invalid Option Text" });
     }
+  }
 
-    if (existingPoll.Options.length < 2) {
-        return res.status(400).json({ message: 'At least 2 options are required' });
-    }
+  if (
+    !existingPoll.ExpirationTime ||
+    existingPoll.ExpirationTime <= new Date()
+  ) {
+    return res.status(400).json({ message: "Invalid Poll Expiration Date" });
+  }
 
-    for (const option of existingPoll.Options) {
-        if (!option.OptionText || option.OptionText.length == 0) {
-            return res.status(400).json({ message: 'Invalid Option Text' });
-        }
-    }
+  try {
+    // Save to database
+    await db.updatePoll(existingPoll);
 
-    if (!existingPoll.ExpirationTime || existingPoll.ExpirationTime <= new Date()) {
-        return res.status(400).json({ message: 'Invalid Poll Expiration Date' });
-    }
-
-    try {
-        // Save to database
-        await db.updatePoll(existingPoll);
-
-        // Return the updated Poll
-        res.status(201).json(existingPoll);
-    } catch (err) {
-        res.status(500).json({ message: 'Internal Server Error' });
-    }
+    // Return the updated Poll
+    res.status(201).json(existingPoll);
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
 
 // Middleware to check JWT token
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Extract token from 'Bearer <token>'
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Extract token from 'Bearer <token>'
 
-    if (!token) {
-        return res.status(403).json({ message: 'Access Token is required' });
+  if (!token) {
+    return res.status(403).json({ message: "Access Token is required" });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
     }
-
-    jwt.verify(token, JWT_SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({ message: 'Invalid token' });
-        }
-        req.user = user; // Add user info to request
-        next(); // Proceed to the next middleware or route handler
-    });
+    req.user = user; // Add user info to request
+    next(); // Proceed to the next middleware or route handler
+  });
 }
 
 // Async function to get user by email from database
 async function getUserByEmail(email) {
-    const user = await db.findUserByEmail(email);
-    return user;
+  const user = await db.findUserByEmail(email);
+  return user;
 }
--- selected/backend/db.js
+++ selected/backend/db.js
@@ -1,17 +1,21 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 // Create a MySQL connection pool
+
+//const sqlConnectionString = process.env(sqlConnection);
+//const db.createPool(sqlConnectionString).promise();
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,      // Your MySQL host
-  user: process.env.DB_USER,           // Your MySQL username
-  password: process.env.DB_PASSWORD,   // Your MySQL password
-  database: process.env.DB_NAME // Your MySQL database name
+  host: process.env.DB_HOST, // Your MySQL host
+  user: process.env.DB_USER, // Your MySQL username
+  password: process.env.DB_PASSWORD, // Your MySQL password
+  database: process.env.DB_NAME, // Your MySQL database name
 });
 
 // Function to query the database
 async function findUserByEmail(email) {
-  const query = 'SELECT * FROM User WHERE EmailID = ?';
+  const query = "SELECT * FROM User WHERE EmailID = ?";
 
   return new Promise((resolve, reject) => {
     db.query(query, [email], (err, results) => {
@@ -24,7 +28,8 @@ async function findUserByEmail(email) {
 }
 
 async function savePasscode(userId, passcode) {
-  const query = 'SELECT * FROM PassCode WHERE UserId = ? AND ExpirationDateTime > NOW()';
+  const query =
+    "SELECT * FROM PassCode WHERE UserId = ? AND ExpirationDateTime > NOW()";
 
   return new Promise((resolve, reject) => {
     db.query(query, [userId], (err, results) => {
@@ -33,7 +38,8 @@ async function savePasscode(userId, passcode) {
       }
 
       if (results.length > 0) {
-        const updateQuery = 'UPDATE PassCode SET ExpirationDateTime = DATE_ADD(NOW(), INTERVAL -5 MINUTE) WHERE UserId = ?';
+        const updateQuery =
+          "UPDATE PassCode SET ExpirationDateTime = DATE_ADD(NOW(), INTERVAL -5 MINUTE) WHERE UserId = ?";
         db.query(updateQuery, [userId], (err) => {
           if (err) {
             return reject(err);
@@ -41,7 +47,8 @@ async function savePasscode(userId, passcode) {
           resolve();
         });
       } else {
-        const insertQuery = 'INSERT INTO PassCode (UserId, Code, ExpirationDateTime) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 15 MINUTE))';
+        const insertQuery =
+          "INSERT INTO PassCode (UserId, Code, ExpirationDateTime) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 15 MINUTE))";
         db.query(insertQuery, [userId, passcode], (err) => {
           if (err) {
             return reject(err);
@@ -54,7 +61,8 @@ async function savePasscode(userId, passcode) {
 }
 
 async function isPasscodeValid(userId, passcode) {
-  const query = 'SELECT * FROM PassCode WHERE UserId = ? AND Code = ? AND ExpirationDateTime > NOW()';
+  const query =
+    "SELECT * FROM PassCode WHERE UserId = ? AND Code = ? AND ExpirationDateTime > NOW()";
 
   return new Promise((resolve, reject) => {
     db.query(query, [userId, passcode], (err, results) => {
@@ -63,7 +71,8 @@ async function isPasscodeValid(userId, passcode) {
       }
 
       // Update the record to expire the passcode
-      const updateQuery = 'UPDATE Passcode SET ExpirationDateTime = DATE_ADD(NOW(), INTERVAL -5 MINUTE) WHERE UserId = ? AND Code = ?';
+      const updateQuery =
+        "UPDATE Passcode SET ExpirationDateTime = DATE_ADD(NOW(), INTERVAL -5 MINUTE) WHERE UserId = ? AND Code = ?";
       db.query(updateQuery, [userId, passcode], (err) => {
         if (err) {
           return reject(err);
@@ -76,7 +85,7 @@ async function isPasscodeValid(userId, passcode) {
 }
 
 async function getActivePolls() {
-  const query = 'SELECT * FROM Poll WHERE ExpirationDateTime > NOW()';
+  const query = "SELECT * FROM Poll WHERE ExpirationDateTime > NOW()";
 
   return new Promise((resolve, reject) => {
     db.query(query, (err, results) => {
@@ -90,53 +99,66 @@ async function getActivePolls() {
 
 // Save new User to the database
 async function saveUser(user) {
-  const query = 'INSERT INTO User (FirstName, LastName, EmailID) VALUES (?, ?, ?)';
+  const query =
+    "INSERT INTO User (FirstName, LastName, EmailID) VALUES (?, ?, ?)";
 
   return new Promise((resolve, reject) => {
-    db.query(query, [user.firstName, user.lastName, user.emailId], (err, results) => {
-      if (err) {
-        return reject(err);
+    db.query(
+      query,
+      [user.firstName, user.lastName, user.emailId],
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(results);
       }
-      resolve(results);
-    });
-  });
-};
-
-async function savePoll(poll, userId) {
-  const query = 'INSERT INTO Poll (QuestionText, UserId, ExpirationDateTime) VALUES (?, ?, ?)';
-
-  return new Promise((resolve, reject) => {
-    db.query(query, [poll.QuestionText, userId, poll.ExpirationTime], (err, results) => {
-      if (err) {
-        return reject(err);
-      }
-
-      const pollId = results.insertId;
-
-      const optionsQuery = 'INSERT INTO PollOption (PollId, OptionText) VALUES (?, ?)';
-      const promises = poll.Options.map(option => {
-        return new Promise((resolve, reject) => {
-          db.query(optionsQuery, [pollId, option], (err) => {
-            if (err) {
-              return reject(err);
-            }
-            resolve();
-          });
-        });
-      });
-
-      Promise.all(promises)
-        .then(() => resolve(results))
-        .catch(reject);
-    });
+    );
   });
 }
 
-module.exports = { 
-  findUserByEmail, 
-  savePasscode, 
-  isPasscodeValid, 
+module.exports = { findUserByEmail };
+
+async function savePoll(poll, userId) {
+  const query =
+    "INSERT INTO Poll (QuestionText, UserId, ExpirationDateTime) VALUES (?, ?, ?)";
+
+  return new Promise((resolve, reject) => {
+    db.query(
+      query,
+      [poll.QuestionText, userId, poll.ExpirationTime],
+      (err, results) => {
+        if (err) {
+          return reject(err);
+        }
+
+        const pollId = results.insertId;
+
+        const optionsQuery =
+          "INSERT INTO PollOption (PollId, OptionText) VALUES (?, ?)";
+        const promises = poll.Options.map((option) => {
+          return new Promise((resolve, reject) => {
+            db.query(optionsQuery, [pollId, option], (err) => {
+              if (err) {
+                return reject(err);
+              }
+              resolve();
+            });
+          });
+        });
+
+        Promise.all(promises)
+          .then(() => resolve(results))
+          .catch(reject);
+      }
+    );
+  });
+}
+
+module.exports = {
+  findUserByEmail,
+  savePasscode,
+  isPasscodeValid,
   getActivePolls,
   savePoll,
-  saveUser
+  saveUser,
 };
