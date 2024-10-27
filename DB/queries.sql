850528a3cd3f12b81a89a4ee70c2b7ebdbc37c12
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-10-27 13:58:19 -0500

    updated db for comments and voting

--- DB/queries.sql
+++ DB/queries.sql
@@ -1,1 +1,18 @@
-select * from User;
+-- SELECT PollId, COUNT(*) FROM PollAnswer GROUP BY PollId;
+-- SELECT UserId, COUNT(*) FROM Poll GROUP BY UserId;
+-- SELECT OptionText, PollOptionId FROM PollOption WHERE PollId=1;
+-- SELECT CommentID, Content, UserId, CreatedDate FROM Comment WHERE PollID=5;
+SELECT * FROM Comment;
+
+-- DROP TABLE PollAnswer;
+-- CREATE TABLE PollAnswer (
+--     PollAnswerId BIGINT AUTO_INCREMENT PRIMARY KEY,
+--     PollId BIGINT,
+--     OptionId BIGINT,
+--     UserId BIGINT,
+--     CreatedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     UNIQUE (PollId, OptionId, UserId),
+--     FOREIGN KEY (PollId) REFERENCES Poll(PollId) ON DELETE RESTRICT,
+--     FOREIGN KEY (OptionId) REFERENCES PollOption(PollOptionId) ON DELETE RESTRICT,
+--     FOREIGN KEY (UserId) REFERENCES User(UserId) ON DELETE RESTRICT
+-- );
